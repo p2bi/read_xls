cbb1dffe1c403491596b706e48aa3a940244b223
Jeff Taggart <jeff@jetaggart.com>
2016-02-26 13:21:52 -0700

    Fill in empty rows

--- lib/read_xls/workbook/worksheet_builder.rb
+++ lib/read_xls/workbook/worksheet_builder.rb
@@ -4,7 +4,7 @@ module ReadXls
       attr_accessor :rows, :sst
 
       def initialize
-        self.rows = {}
+        self.rows = []
       end
 
       def add_row(row_index, row)
@@ -17,8 +17,19 @@ module ReadXls
       end
 
       def build
-        evaluated_rows = rows.values.map { |r| r.evaluate }
+        evaluated_rows = build_rows
         ::ReadXls::Workbook::Worksheet.new(:rows => evaluated_rows)
+      end
+
+
+      private
+
+      def build_rows
+        rows.each_with_index.each do |_, row_index|
+          rows[row_index] ||= ::ReadXls::Workbook::Row.new(row_index, 0, 0)
+        end
+
+        rows.map { |r| r.evaluate }
       end
     end
   end
--- spec/read_xls_spec.rb
+++ spec/read_xls_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 describe ReadXls do
   describe ".parse" do
-    it "returns the correct number of rows" do
+    it "parses simple" do
       spreadsheet = ReadXls.parse(
-        get_spreadsheet_path("simple_no_headers.xls")
+        get_spreadsheet_path("simple.xls")
       )
 
       expect(spreadsheet.sheets.length).to eq(1)
@@ -13,6 +13,19 @@ describe ReadXls do
       expect(spreadsheet.sheets.first.rows[1]).to eq(["monkey", "bear", "giraffe", "eagle"])
       expect(spreadsheet.sheets.first.rows[2]).to eq(["sara", "john", "nick", "nicole"])
       expect(spreadsheet.sheets.first.rows[3]).to eq([true, false, true, false])
+    end
+
+    it "parses row_spacing" do
+      spreadsheet = ReadXls.parse(
+        get_spreadsheet_path("row_spacing.xls")
+      )
+
+      expect(spreadsheet.sheets.length).to eq(1)
+      expect(spreadsheet.sheets.first.rows.length).to eq(4)
+      expect(spreadsheet.sheets.first.rows[0]).to eq(["a", "b", "c", "d"])
+      expect(spreadsheet.sheets.first.rows[1]).to eq([])
+      expect(spreadsheet.sheets.first.rows[2]).to eq([])
+      expect(spreadsheet.sheets.first.rows[3]).to eq(["e", "f", "g", "h"])
     end
   end
 
